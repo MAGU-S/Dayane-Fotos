2f5d04e1a9e1b4643f5a5942c217e362eace50f6
Nerd-MHOV <84547045+Nerd-MHOV@users.noreply.github.com>
2021-06-05 22:26:18 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 	include('config.php');
+	$url = isset($_GET['url']) ? $_GET['url'] : 'home';
 ?>
 <!DOCTYPE html>
 <html>
@@ -15,12 +16,15 @@
 	<link rel="icon" href="<?php echo INCLUDE_PATH; ?>favicon.ico" type="image/x-icon" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"><!--deixar o site responsivo-->
 	<meta name="keywords" content="fotos-tumblr-foto-para-perfil-registro-aleatoria-antiga-animada-a-mais-curtida-do-instagram-fotografia-e-o-texto-remetem--influência-do-estilo-retrata-conhecida-álbum-album-de-ás-como-fotografar-á-noite-máquinas-fotograficas-à-fotografa-bonita-bts-bolo-noite-feste-casal-colorida-capa-facebook-cachorro-colagem-c-namorado-frases-flores-bebê-engraçada-em-pdf-perfil-gente-tirou-instagram-youtube-google-photos-photo-image-imagem-jovem-casamento-recem-nascido-gravida-albuns-de-figurinhas-presente-dia-dos-presenteie-fotofigurinha">
-	<meta name="description" href="Site De Fotos da Dayane, Contrate o nosso serviço, veja alguns dos meus ensaios e trabalhos, e conheça nosso Álbum Fotofigurinha."> 
+	<meta name="description" href="Site De Fotos da Dayane, Contrate os nossos serviços, veja alguns dos meus ensaios fotográficos e trabalhos, e conheça nosso 'Álbum Fotofigurinha'."> 
 	
 
 	
 </head>
 <body>
+
+	<base base = "<?php INCLUDE_PATH; ?>" />
+	
 	<header>
 		<div class="header1">
 			<div class="center">
@@ -45,14 +49,21 @@
 			<div class="center">
 				<nav>
 					<ul>
-						<li><a class="active-menu" href="">HOME</a></li>
-						<li><a href="">ÁLBUNS</a></li>
-						<li><a href="">CONTATO</a></li>
+						<li><a <?php activeMenu('home'); ?> href="<?php INCLUDE_PATH; ?>home">HOME</a></li>
+						<li><a <?php activeMenu('albuns'); ?> href="<?php INCLUDE_PATH; ?>albuns">ÁLBUNS</a></li>
+						<li><a <?php activeMenu('contato'); ?> href="<?php INCLUDE_PATH; ?>contato">CONTATO</a></li>
 					</ul>
 				</nav>
 			</div><!-- center -->
 		</div><!-- header2 -->
-
+<div class="clear"></div>
 	</header>
+	<?php 
+		if(file_exists('pages/'.$url.'.php')){
+			include('pages/'.$url.'.php');
+		}else{
+			include('pages/404.php'); 
+		}
+	?>
 </body>
 </html>

04f9b47c9ebdedbf052decbda178bcfc9d6edf4e
Gustavo Furlanetto <gustavo.furlanetto@unesp.br>
2021-06-06 17:12:13 -0300

    Add files via upload

--- pages/404.php
+++ pages/404.php
@@ -1,1 +1,10 @@
-<h2>ERRO</h2>
+<section class="mensagem-erro404">
+	<div class="center">
+		<div class="Wrapper404">
+			<h1>404</h1>
+			<h5>Desculpe, não conseguimos encontrar a página desejada</h5>
+			<h6><a href="<?php echo INCLUDE_PATH; ?>">Voltar a página inicial</a></h6>
+		</div><!--Wrapper404-->
+	</div><!--Center-->
+</section>
+

4629de366deb9a6b1470722c1f17c95cded0ec61
Nerd-MHOV <matheus.henrique4245@gmail.com>
2021-06-09 15:28:03 -0300

    home and background up

--- pages/Home.php
+++ pages/Home.php
@@ -1,1 +1,14 @@
-<h2>HOME</h2>
+<section class="banner-container">
+		<div class="overlay"></div><!-- overlay -->
+			<div class="box-container center">
+				
+				<div class="logo"><img src="imagens/logo1.png"></div>
+				<p>Precisando de um Fotografo?</p>
+				<p>entre em contato!</p>
+				<input type="submit" name="ação" value="Contato!">
+				
+			</div><!-- box-container -->
+</section><!--banner-container-->
+<section class="conteudo">
+	teste
+</section><!-- conteudo -->
